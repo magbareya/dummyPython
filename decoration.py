4f9700edc79b53f80edea942148513921a3d7dd4
Mahmoud Agbareya <mahmoud.agbareya@gmail.com>
2017-05-21 13:48:13 +0300

    add files

---
+++ decoration.py
@@ -0,0 +1,36 @@
+def our_decorator(func):
+    def function_wrapper(*args, **keys):
+        print(args)
+        print(keys)
+        return func(*args, **keys)
+    print("decorator was called on function %s"%func.__name__)
+    function_wrapper.__name__ = func.__name__
+    function_wrapper.__doc__ = func.__doc__
+    function_wrapper.__module__ = func.__module__
+    return function_wrapper
+
+@our_decorator
+def printMsg():
+    print("hello world!")
+
+@our_decorator
+def succ(n):
+    return n + 1
+
+@our_decorator
+def add(x,y):
+    return x+y
+
+@our_decorator
+def ss(list):
+    return sum(list)
+
+
+def main():
+    print("printMsg() = %s" % printMsg())
+    print("succ(1) = %s"%succ(1))
+    print("add(1,2) = %s" % add(1,2))
+    print("ss([1,2,3]) = %s" % ss([1,2,3]))
+
+
+if __name__ == "__main__": main()
