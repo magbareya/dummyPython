6a242d20a66309d352e78bb8c10abc41eefaad6a
Mahmoud Agbareya <mahmoud.agbareya@gmail.com>
2017-06-08 10:17:46 +0300

    try committing

--- dummyScript.py
+++ dummyScript.py
@@ -1,7 +1,18 @@
+import sys
+import logging
 
 
 def main():
-    pass
+    logging.basicConfig(format=u'%(filename)s:%(lineno)d:%(levelname)s [%(asctime)s]: %(message)s')
+    log = logging.getLogger('Tracing')
+    log.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler('Tracing.log')
+    file_handler.setLevel(logging.DEBUG)
+    log.addHandler(file_handler)
+
+    t = (5, 60, 334)
+
+    log.info('%d  %d  %d', *t)
 
 
 if __name__ == '__main__':
--- my_math.py
+++ my_math.py
@@ -1,13 +1,13 @@
+import sys
 from unittest import TestCase
 from my_mathh import MyMath
-import sys
 
 
 class MyTests(TestCase):
 
     def test_with_tracer(self):
+        print('original tracer = ', id(sys.gettrace()))
         old_tracer = sys.gettrace()
-
         def my_tracer(frame, event, arg):
             nonlocal old_tracer
             print('my tracer was called')
@@ -22,12 +22,18 @@ class MyTests(TestCase):
             print('_____________')
             if old_tracer is not None:
                 old_tracer(frame, event, arg)
+            if sys.gettrace() != my_tracer:
+                sys.settrace(my_tracer)
+            print(id(sys.gettrace()))
             return my_tracer
 
+        print(id(sys.gettrace()))
         sys.settrace(my_tracer)
+        print('my_trace id = ', id(my_tracer))
+        print(id(sys.gettrace()))
         calc = MyMath()
-        sys.settrace(my_tracer)
         assert calc.power(2,2) == 4
-        sys.settrace(my_tracer)
+        print(id(sys.gettrace()))
         assert calc.mul(2,3) == 6
-        sys.settrace(my_tracer)
+        print(id(sys.gettrace()))
+        sys.settrace(old_tracer)
